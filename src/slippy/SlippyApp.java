c8eb85959b7100b389bd2f4a9a1915b570a7e3e1
mlouthan <louthanm@gmail.com>
2019-09-14 22:48:26 -0700

    Cleaned input scene up a bit, added support for enter to be used as a method of pressing play, shift enter now appends a line

--- src/slippy/SlippyApp.java
+++ src/slippy/SlippyApp.java
@@ -1,13 +1,13 @@
 package slippy;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -22,38 +22,54 @@ public class SlippyApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
         VideoPanel videoStage = new VideoPanel();
         videoStage.initStyle(StageStyle.UNDECORATED);
         videoStage.setTitle("Slippy");
         videoStage.show();
 
-        Group inputRoot = new Group();
-        GridPane grid = new GridPane();
-        grid.setHgap(10);
-        grid.setVgap(10);
-        slippyTextInput = new TextArea();
-        slippyTextInput.setPrefRowCount(3);
-        slippyTextInput.setPrefColumnCount(20);
-        slippyTextInput.setWrapText(true);
-        slippyTextInput.setPromptText("Slippy says");
-        grid.add(new Label("Slippy says:"), 0, 0);
-        grid.add(slippyTextInput, 1, 0, 1, 2);
-        Button playButton = new Button("Play");
-        grid.add(playButton, 0, 1);
-        inputRoot.getChildren().add(grid);
-        Scene inputScene = new Scene(inputRoot, 400, 75);
+        Scene inputScene = createSlippyInputScene(videoStage);
         primaryStage.setScene(inputScene);
         primaryStage.initStyle(StageStyle.DECORATED);
         primaryStage.setTitle("What does Slippy say?");
         primaryStage.show();
 
-        EventHandler<ActionEvent> event = e -> videoStage.play(slippyTextInput.getText());
-        playButton.setOnAction(event);
-
         primaryStage.setOnCloseRequest(event1 -> videoStage.close());
         videoStage.setOnCloseRequest(event1 -> primaryStage.close());
+    }
+
+    private Scene createSlippyInputScene(final VideoPanel videoPanel) {
+        slippyTextInput = new TextArea();
+        slippyTextInput.setPrefRowCount(3);
+        slippyTextInput.setPrefColumnCount(20);
+        slippyTextInput.setWrapText(true);
+        slippyTextInput.setPromptText("Slippy says");
+
+        Button playButton = new Button("Play");
+        playButton.setDefaultButton(true);
+
+        slippyTextInput.addEventFilter(KeyEvent.KEY_PRESSED, ev -> {
+            if (ev.getCode() == KeyCode.ENTER) {
+                if (ev.isShiftDown()) {
+                    slippyTextInput.appendText("\n");
+                } else {
+                    playButton.fire();
+                }
+                ev.consume();
+            }
+        });
+        playButton.setOnAction(e -> videoPanel.play(slippyTextInput.getText()));
+
+        Group inputRoot = new Group();
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.add(new Label("Slippy says:"), 0, 0);
+        grid.add(slippyTextInput, 1, 0, 1, 2);
+        grid.add(playButton, 0, 1);
+        inputRoot.getChildren().add(grid);
+
+        return new Scene(inputRoot, 400, 75);
     }
 
 
